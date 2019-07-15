83aa212640cbeb03c510a5b02f79bafcff231baa
Geoff Register <geoff.register@smashingboxes.com>
2019-07-15 11:08:34 -0400

    first commit

---
+++ app/javascript/components/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Userform } from './Userform'
+
+function App() {
+    return (
+        <div>
+            Boone's Farm!
+        </div>
+    )
+}
+
+export default App;
+
+// class App extends React.Component {
+//     render () {
+//         return (
+//             <div>Hello, Mang!</div>
+//         )
+//     }
+// }
+
+// export default App
